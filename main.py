2c55873c27a4e46e2bc40ae20e69be1117121908
LouiseLiz <91270021+LouiseLiz@users.noreply.github.com>
2025-04-26 19:14:03 -0300

    Cadastrando e listando produtos

--- main.py
+++ main.py
@@ -25,13 +25,74 @@ product_price = st.number_input("Preço do Produto", min_value=0.0, format="%.2f
 product_description = st.text_area("Descrição do Produto")
 product_image = st.file_uploader("Imagem do Produto", type=["png", "jpg", "jpeg"])
 
-if st.button ('Salvar Produto'):
-        return_message = 'Produto salvo com sucesso'
+# Salvando img Blob
+def upload_blob(file):
+        blob_service_client = BlobServiceClient.from_connection_string(blobConnectionString)
+        container_client = blob_service_client.get_container_client(blobContainerName)
+        blob_name = f"{uuid.uuid4()}{os.path.splitext(file.name)[1]}"
+        blob_client = container_client.get_blob_client(blob_name)
+        blob_client.upload_blob(file.read(), overwrite=True)
+        imagem_url = f"https://{blobaccountName}.blob.core.windows.net/{blobContainerName}/{blob_name}"
+        return imagem_url
+     
+
+def insert_product_sql(product_name, product_price, product_description, product_image):
+    try:
+        imagem_url = upload_blob(product_image)          
+        conn = pymssql.connect(server=SQL_SERVER, user=SQL_USER, password=SQL_PASSWORD, database=SQL_DATABASE)
+        cursor = conn.cursor()
+        cursor.execute(
+            "INSERT INTO Produtos (nome, preco, descricao, imagem_url) VALUES (%s, %s, %s, %s)",
+            (product_name, product_price, product_description, imagem_url)
+        )
+        conn.commit()
+        conn.close()
+        return True
+    except Exception as e:
+        st.error(f"Erro ao inserir produto {e}")
+        return False
+
+ 
+
+if st.button('Cadastrar Produto'):
+    if product_name and product_price and product_description and product_image:
+        success = insert_product_sql(product_name, product_price, product_description, product_image)
+        if success:
+            st.success('Produto cadastrado com sucesso!')
+    else:
+        st.warning('Por favor, preencha todos os campos.')
+
+
+
 
 st.header('Produtos cadastrados')
 
-if st.button('Listar Produtos'):
-        return_message = 'Produtos Listados com Sucesso!'
 
+def list_products_sql():
+    try:
+        conn = pymssql.connect(server=SQL_SERVER, user=SQL_USER, password=SQL_PASSWORD, database=SQL_DATABASE)
+        cursor = conn.cursor()
+        cursor.execute("SELECT nome, preco, descricao, imagem_url FROM Produtos")
+        products = cursor.fetchall()
+        conn.close()
+        return products
+    except Exception as e:
+        st.error(f"Erro ao buscar produtos: {e}")
+        return []
+def list_produtos_screen():
+    products = list_products_sql()
+    if products:
+        for product in products:
+            product_name, product_price, product_description, imagem_url = product
 
+            # Exibe os detalhes de cada produto
+            st.subheader(product_name)
+            st.write(f"Preço: R${product_price}")
+            st.write(f"Descrição: {product_description}")
+            st.image(imagem_url, width=200)  # Exibe a imagem a partir da URL do Azure Blob
 
+    else:
+        st.warning('Nenhum produto encontrado.')
+               
+if st.button("Listar Produtos"):
+    list_produtos_screen()        
